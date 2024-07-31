8679e7c6aee534410a5e142b5fa7fd3bf9b484a9
g.wedge <g.wedge@gmail.com>
2024-07-31 10:55:21 +0000

    busses nr 7 added to workday summer

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,43 @@ import "moment/dist/locale/sr";
 import { summer } from "./data/Summer/busScheduleSummer";
 import { getTodayType } from "./utilFn/getDayType";
 import { getDepartures } from "./utilFn/getDepartures";
+import { useState } from "react";
 
 function App() {
   moment.locale("sr");
+  const [busTypeSelected,setBusTypeSelected]=useState({
+    5:true,
+    7:false
+  })
   const allBussesData = summer[getTodayType()];
   const todaysDay = moment().format("dddd");
   const getDateType = todaysDay.charAt(0).toUpperCase() + todaysDay.slice(1);
+  const isSelected = (buttonLabel:5 | 7) => {
+    return Boolean(busTypeSelected[buttonLabel])
+  }
 
+  const filterFn = (allBusses) => {
+    let filteredBusses = allBusses.filter((bus) => {
+        return String(bus.bus).includes('5') && busTypeSelected[5] || String(bus.bus).includes('7') && busTypeSelected[7]
+    });
+    return filteredBusses
+  };
   return (
     <div className="app-wrapper">
       <div className="date">{moment().format("lll")}</div>
+      <div className="second-row">
+        <button className={isSelected(5) ? "is-selected" : ""} onClick={() => {
+          setBusTypeSelected((prev) => ({...prev,5:!prev[5]}) )
+        }}>5*</button>
       <div className="date-type">{getDateType}</div>
+        <button className={isSelected(7) ? "is-selected" : ""} onClick={() => {
+          setBusTypeSelected((prev) => ({...prev,7:!prev[7]}) )
+        }} >7*</button>
+      </div>
       <div className="departures">
-        {getDepartures(allBussesData).map((departure) => {
+        {filterFn(getDepartures(allBussesData)).map((departure) => {
           return (
-            <div className="departure" key={departure.time}>
+            <div className="departure" key={departure.bus + departure.time}>
               <span style={{ fontWeight: "bold" }}>{departure.bus}</span>
               <span>{departure.time}</span>
             </div>
